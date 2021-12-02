f08103c8c88e70e4f00aefd7ba66f69cf67c1798
Ryan <technogamers543@gmail.com>
2021-12-02 14:28:24 +0800

    making item function/future but still unfinished and unperfect

--- item-shop.cpp
+++ item-shop.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 	char buyitem;
 	char buyacces;
 	char buyweapon;
+	char con;
 	int count;
 
 	while (true) {
@@ -32,7 +33,42 @@ int main(int argc, char *argv[])
 			cin >> buy;
 		}
 
-		if (buy == '4') {
+		if (buy == '1') {
+			while (true) {
+				puts("what do you want\n");
+
+				puts("1. health capsul\t DRP.1000");
+				puts("2. stamina capsul\t DRP.2000");
+				puts("3. fried indomie\t DRP.10000");
+				puts("4. energy drink\t DRP.5500");
+				puts("5. revive chip\t DRP.30000\n");
+				puts("6. back");
+
+				printf(":");
+				cin >> buyitem;
+
+				if (buyitem == '1') {
+					puts("health capsul\n");
+					puts("HP: 30\n");
+
+					puts("do you want buy it\n");
+					printf("y/n: ");
+					cin >> con;
+
+					if (con == 'y') {
+						printf("How many: ");
+						cin >> count;
+						int total = count * 1000;
+
+						printf("you buy %d health capsul with price: %d\n", count, total);
+					}
+				} 
+				else if (buyitem == '6') {
+					break;
+				}
+			}
+		}
+		else if (buy == '4') {
 			break;
 		}
 	}
