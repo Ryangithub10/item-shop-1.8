f7955750b751344ac8e4e925b92f70f19c62c011
Ryan <technogamers543@gmail.com>
2021-12-04 21:55:54 +0800

    update for item future | finished

--- item-shop.cpp
+++ item-shop.cpp
@@ -21,9 +21,62 @@ void selectI(char x) {
 			printf("how many: ");
 			cin >> count;
 			total = count * 1000;
-			printf("you buy %d health capsul with price: DRP.%d\n\n", count, total);
+			printf("you buy %d health capsul with price: RP.%d\n\n", count, total);
 		}
 	}
+	else if (x == '2') {
+		puts("stamina capsul\n");
+		puts("SP: +20\n");
+		printf("do you want buy it y/n: ");
+		cin >> con;
+
+		if (con == 'y') {
+			printf("how many: ");
+			cin >> count;
+			total = count * 2000;
+			printf("you buy %d stamina capsul with price: RP.%d\n\n", count, total);
+		}
+	}
+	else if (x == '3') {
+		puts("Indomie goreng\n");
+		puts("HP: +100\n");
+		printf("do you want buy it y/n: ");
+		cin >> con;
+
+		if (con == 'y') {
+			printf("how many: ");
+			cin >> count;
+			total = count * 10000;
+			printf("you buy %d Indomie goreng with price: RP.%d\n\n", count, total);
+		}
+	}
+	else if (x == '4') {
+		puts("energy drink\n");
+		puts("HP: +30\t SP: +40\n");
+		printf("do you want buy it y/n: ");
+		cin >> con;
+
+		if (con == 'y') {
+			printf("how many: ");
+			cin >> count;
+			total = count * 5000;
+			printf("you buy %d energy drink with price: RP.%d\n\n", count, total);
+		}
+	}
+	else if (x == '5') {
+		puts("revive chip\n");
+		puts("reviving a your party and give max HP after reviving\n");
+		printf("do you want buy it y/n: ");
+		cin >> con;
+
+		if (con == 'y') {
+			printf("how many: ");
+			cin >> count;
+			total = count * 20000;
+			printf("you buy %d revive chip with price: RP.%d\n\n", count, total);
+		}
+	}
+
 }
 
 void selectA(char x) {
@@ -41,7 +94,7 @@ void selectA(char x) {
 
 			total = count * 5000;
 
-			printf("you buy %d ring exp with price: DRP.%d\n\n", count, total);
+			printf("you buy %d ring exp with price: RP.%d\n\n", count, total);
 		}
 	}
 }
@@ -51,11 +104,11 @@ void item() {
 	while (true) {
 		puts("what do you want\n");
 
-		puts("1. health capsul\t DRP.1000");
-		puts("2. stamina capsul\t DRP.2000");
-		puts("3. indomie goreng\t DRP.10000");
-		puts("4. energy drink\t\t DRP.5000");
-		puts("5. revive chip\t\t DRP.20000");
+		puts("1. health capsul\t RP.1000");
+		puts("2. stamina capsul\t RP.2000");
+		puts("3. indomie goreng\t RP.10000");
+		puts("4. energy drink\t\t RP.5000");
+		puts("5. revive chip\t\t RP.20000");
 		puts("6. back\n");
 
 		printf(":");
@@ -77,9 +130,9 @@ void acces() {
 
 		puts("what do you want\n");
 
-		puts("1. ring exp\t\t DRP.5000");
-		puts("2. digital vest\t\t DRP.13000");
-		puts("3. stamina capsul pouch\t DRP.8000");
+		puts("1. ring exp\t\t RP.5000");
+		puts("2. digital vest\t\t RP.13000");
+		puts("3. stamina capsul pouch\t RP.8000");
 		puts("4. back\n");
 
 		printf(":");
